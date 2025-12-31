9984871e66a989ac237ff98458b4b47b207fc4bd
Anderson Patricio <andersongpatricio@hotmail.com>
2025-12-31 11:38:41 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import ViteFonts from 'unplugin-fonts/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/, // .vue
+        /\.vue\?vue/, // .vue?vue
+        /\.md$/ // .md
+      ],
+      imports: [
+        'vue'
+      ]
+    }),
+    vue(),
+    vueDevTools(),
+    ViteFonts({
+      fontsource: {
+        families: [
+          {
+            name: 'Roboto',
+            weights: [100, 300, 400, 500, 700, 900],
+            styles: ['normal', 'italic'],
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
